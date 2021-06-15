c9a3b05c03a6bee0f68f29912b2b32923db73b48
Marat.Serhiyevich <sergievich1911@gmail.com>
2021-06-15 12:58:07 +0300

    refactor: set up unit tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,135 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { getMockUsers } from '../users/mock/mockUsers';
+import { UserEntity } from '../users/user.entity';
+import { UsersService } from '../users/users.service';
+import { BadRequestException, UnauthorizedException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let userService: UsersService;
+  let jwtService: JwtService;
+
+  const mockUsers = getMockUsers();
+  const findUser: jest.Mock = jest.fn().mockResolvedValue(mockUsers[0]);
+  const saveUser: jest.Mock = jest.fn().mockResolvedValue(mockUsers[0]);
+
+  const UsersRepository = {
+    findOne: findUser,
+    save: saveUser,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        UsersService,
+        {
+          useValue: UsersRepository,
+          provide: getRepositoryToken(UserEntity),
+        },
+        {
+          provide: JwtService,
+          useValue: {},
+        },
+        {
+          provide: ConfigService,
+          useValue: {},
+        },
+      ],
     }).compile();
-
+    jwtService = module.get<JwtService>(JwtService);
+    userService = module.get<UsersService>(UsersService);
     service = module.get<AuthService>(AuthService);
+  });
+
+  it('should validate user', async () => {
+    userService.getUserByName = jest.fn().mockResolvedValue(mockUsers[0]);
+    service.comparePassword = jest.fn().mockResolvedValue(true);
+    expect(
+      await service.validateUser(mockUsers[0].name, mockUsers[0].password),
+    ).toBe(mockUsers[0]);
+  });
+
+  it('should compare password', async () => {
+    expect(
+      await service.comparePassword(
+        'asddaaaaa',
+        '$2a$08$FmJG.inuMJa0Ui/59UO.2uEKGwAb2CZU/RU2L/Ody3134M6jMfZSC',
+      ),
+    ).not.toBeNull();
+  });
+
+  it('should hash password', async () => {
+    expect(await service.hashPassword('asddaaaaa')).not.toBe('asddaaaaa');
+  });
+
+  it('should create token', async () => {
+    jwtService.signAsync = jest.fn().mockResolvedValue('token');
+    expect(await service.getJwtToken(mockUsers[0].id)).not.toBeNull();
+  });
+
+  it('should validate oauth login', async () => {
+    userService.getUserByThirdPartyId = jest
+      .fn()
+      .mockResolvedValue(mockUsers[0]);
+    userService.createUserFromOAuth = jest.fn().mockResolvedValue(mockUsers[0]);
+    service.getJwtToken = jest.fn().mockResolvedValue('token');
+    expect(
+      await service.validateOAuthLogin(mockUsers[0], 'google'),
+    ).not.toBeNull();
+  });
+
+  it('should validate oauth login', async () => {
+    userService.getUserByThirdPartyId = jest.fn().mockResolvedValue(null);
+    userService.createUserFromOAuth = jest.fn().mockResolvedValue(mockUsers[0]);
+    service.getJwtToken = jest.fn().mockResolvedValue('token');
+    expect(
+      await service.validateOAuthLogin(mockUsers[0], 'google'),
+    ).not.toBeNull();
+  });
+
+  it('should not validate user', async () => {
+    userService.getUserByName = jest.fn().mockResolvedValue(undefined);
+    service.comparePassword = jest.fn().mockResolvedValue(false);
+    expect(async () => {
+      await service.validateUser(mockUsers[0].name, mockUsers[0].password);
+    }).rejects.toEqual(new UnauthorizedException());
+  });
+
+  it('should not validate user', async () => {
+    userService.getUserByName = jest.fn().mockResolvedValue(mockUsers[0]);
+    service.comparePassword = jest.fn().mockResolvedValue(false);
+    expect(async () => {
+      await service.validateUser(mockUsers[0].name, mockUsers[0].password);
+    }).rejects.toEqual(new UnauthorizedException());
+  });
+
+  it('should return token', async () => {
+    service.getJwtToken = jest.fn().mockResolvedValue('token');
+    const token = await service.login(mockUsers[0]);
+    expect(token).not.toBeNull();
+  });
+
+  it('should register user', async () => {
+    service.hashPassword = jest.fn().mockResolvedValue(mockUsers[0].password);
+    userService.createUser = jest.fn().mockResolvedValue(mockUsers[0]);
+    const user = await service.register(mockUsers[0]);
+    expect(user).toBe(true);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should not register user', async () => {
+    service.hashPassword = jest.fn().mockResolvedValue(mockUsers[0].password);
+    userService.createUser = jest.fn().mockResolvedValue(new Error());
+    expect(async () => {
+      await service.register(mockUsers[0]);
+    }).rejects.toEqual(new BadRequestException());
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserEntity } from './user.entity';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { getMockUsers } from './mock/mockUsers';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let userService: UsersService;
+  const mockUsers = getMockUsers();
 
   beforeEach(async () => {
+    const UsersRepository = {
+      findOne: jest.fn().mockResolvedValue(mockUsers[0]),
+      save: jest.fn().mockResolvedValue(mockUsers[0]),
+      create: jest.fn().mockResolvedValue(mockUsers[0]),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          useValue: UsersRepository,
+          provide: getRepositoryToken(UserEntity),
+        },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    userService = module.get<UsersService>(UsersService);
+  });
+
+  afterEach(async () => {
+    jest.resetAllMocks();
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  it('should create user', async () => {
+    expect(
+      await userService.createUser({
+        name: mockUsers[0].name,
+        password: mockUsers[0].password,
+      }),
+    ).toBe(mockUsers[0]);
+  });
+
+  it('should find user by name', async () => {
+    expect(await userService.getUserByName(mockUsers[0].name)).toBe(
+      mockUsers[0],
+    );
+  });
+
+  it('should find user by id', async () => {
+    expect(await userService.getUserById(mockUsers[0].id)).toBe(mockUsers[0]);
+  });
+
+  it('should create user from oauth', async () => {
+    expect(
+      await userService.createUserFromOAuth(
+        {
+          thirdPartyId: mockUsers[0].thirdPartyId,
+          name: {
+            givenName: 'example',
+            familyName: 'example',
+          },
+        },
+        'google',
+      ),
+    ).toBe(mockUsers[0]);
   });
 });
